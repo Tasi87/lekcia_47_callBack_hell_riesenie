53d3dcb4d5194a5f7307bae59a0be384fa7f8472
Tasi <tasi87@gmail.com>
2026-04-20 12:17:41 +0200

    callBack hell riesenie

--- src/script.js
+++ src/script.js
@@ -1,37 +1,18 @@
-//idea	Asynchronný JavaScript
-
-const button = document.createElement("button")
-button.id = "btn"
-button.textContent = "Klikni"
-document.body.append(button)
-button.style.fontSize = "2rem"
-
-//note	CallBack hell
-
-// 1. príklad
-setTimeout(() => {
-	console.log("Krok 1: pripojenie k databázi...")
-
-	setTimeout(() => {
-		console.log("Krok 2: načítanie užívateľa...")
-
-		setTimeout(() => {
-			console.log("Krok 3: overenie prístupu...")
-
-			setTimeout(() => {
-				console.log("Krok 4: načítanie dát...")
-			}, 1000);
-		}, 1000);
-	}, 1000);
-}, 1000);
-
-// 2. príklad
-getData(function(data){
-	procesData(data, function(processed){
-		saveData(processed, function () {
-			console.log("Hotovo")
+//idea	CallBack hell
+//note	Promise, async/await
+	getData(function(data){
+		procesData(data, function(processed){
+			saveData(processed, function () {
+				console.log("Hotovo!")
+			})
 		})
 	})
-})
 
-//fixme	Riešenie	-	promise, async/await
+//fixme Riešenie
+	//note Promise
+	getData()
+		.then(procesData)
+		.then(saveData)
+		.then( () => console.log("Hotovo!") )
+		.catch( err => console.error("Chyba: ", err) )		//! zachytávanie chýb
+
